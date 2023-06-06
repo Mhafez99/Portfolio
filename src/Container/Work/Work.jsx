b054ed736bb23a6d8a6468e5596cfd3bdb24261e
Mhafez99 <m.hafez5594@gmail.com>
2023-06-06 16:53:27 +0300

    Edition Masonary Layout on Works

--- src/Container/Work/Work.jsx
+++ src/Container/Work/Work.jsx
@@ -4,6 +4,14 @@ import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { client, urlFor } from '../../client';
 import { motion } from 'framer-motion';
 import { AppWrap, MotionWrap } from '../../Wrapper/';
+import Masonary from 'react-masonry-css';
+const breakpointObj = {
+  default: 3,
+  1500: 4,
+  1300: 3,
+  1000: 2,
+  774: 1,
+};
 const Work = () => {
   const [activeFilter, setActiveFilter] = useState('ALL');
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
@@ -58,49 +66,53 @@ const Work = () => {
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className='app__work-portfolio'>
-        {filterWork.map((work, index) => (
-          <div className='app__work-item app__flex' key={index}>
-            <div className='app__work-img app__flex'>
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
-              <motion.div
-                whileHover={{ opacity: [0, 1] }}
-                transition={{
-                  duration: 0.25,
-                  ease: 'easeInOut',
-                  staggerChildren: 0.5,
-                }}
-                className='app__work-hover app__flex'>
-                <a href={work.projectLink} target='_blank' rel='noreferrer'>
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.9] }}
-                    transition={{ duration: 0.25 }}
-                    className='app__flex'>
-                    <AiFillEye />
-                  </motion.div>
-                </a>
-                <a href={work.codeLink} target='_blank' rel='noreferrer'>
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.9] }}
-                    transition={{ duration: 0.25 }}
-                    className='app__flex'>
-                    <AiFillGithub />
-                  </motion.div>
-                </a>
-              </motion.div>
-            </div>
-            <div className='app__work-content app__flex'>
-              <h4 className='bold-text'>{work.title}</h4>
-              <p className='p-text' style={{ marginTop: 10 }}>
-                {work.description}
-              </p>
-              <div className='app__work-tag app__flex'>
-                <p className='p-text'>{work.tags[0]}</p>
+        <Masonary
+          breakpointCols={breakpointObj}
+          className='masonary '>
+          {filterWork.map((work, index) => (
+            <div className='app__work-item app__flex' key={index}>
+              <div className='app__work-img app__flex'>
+                <img src={urlFor(work.imgUrl)} alt={work.name} />
+                <motion.div
+                  whileHover={{ opacity: [0, 1] }}
+                  transition={{
+                    duration: 0.25,
+                    ease: 'easeInOut',
+                    staggerChildren: 0.5,
+                  }}
+                  className='app__work-hover app__flex'>
+                  <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className='app__flex'>
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
+                  <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className='app__flex'>
+                      <AiFillGithub />
+                    </motion.div>
+                  </a>
+                </motion.div>
+              </div>
+              <div className='app__work-content app__flex'>
+                <h4 className='bold-text'>{work.title}</h4>
+                <p className='p-text' style={{ marginTop: 10 }}>
+                  {work.description}
+                </p>
+                <div className='app__work-tag app__flex'>
+                  <p className='p-text'>{work.tags[0]}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </Masonary>
       </motion.div>
     </>
   );
